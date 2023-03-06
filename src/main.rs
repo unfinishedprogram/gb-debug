d06699042793140c5f6acb7d07a6bf141871fa4d
Noah Labrecque <noah.labrecque@gmail.com>
2023-03-06 10:38:55 -0500

    egui init

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+fn main() {
+    console_error_panic_hook::set_once();
+    tracing_wasm::set_as_global_default();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::start_web(
+            "canvas",
+            web_options,
+            Box::new(|cc| Box::new(gb_debug::TemplateApp::new(cc))),
+        )
+        .await
+        .expect("failed to start eframe");
+    });
+}
